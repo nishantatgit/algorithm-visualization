e9b9799f4a01534269c43e94651c770a74ca419d
Nishant Kumar <nishant.kumar.shreevastav@gmail.com>
2021-09-18 10:34:49 +0530

    removed console.log

--- client/utils/domReady.js
+++ client/utils/domReady.js
@@ -2,7 +2,7 @@ function onDOMReady(callback) {
 	if (typeof callback !== 'function') {
 		throw new Error('callback is not a function');
 	}
-	console.log('document.readyState', document.readyState);
+	//console.log('document.readyState', document.readyState);
 	if (document.readyState === 'completed') {
 		callback();
 	} else {
