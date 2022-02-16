89bd63e90d9910ea9cf64911c4dcbb2953b0bf76
Nishant Kumar <nishant.kumar.shreevastav@gmail.com>
2022-02-17 00:28:17 +0530

    added index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var currentUnixTimestamp = new Date().getTime();
-var fs = require('fs');
-var writeStream = fs.createWriteStream('./buildID', 'utf-8');
-writeStream.write(`${currentUnixTimestamp}`, 'utf-8');
+// var currentUnixTimestamp = new Date().getTime();
+// var fs = require('fs');
+// var writeStream = fs.createWriteStream('./buildID', 'utf-8');
+// writeStream.write(`${currentUnixTimestamp}`, 'utf-8');
 module.exports = {
 	target: 'web',
 	entry: {
-		[`animations-${currentUnixTimestamp}`]: './client/bubbleSort.js',
+		bundle : './client/bubbleSort.js',
 	},
 	output: {
 		path: `${__dirname}/public`,
