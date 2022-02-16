50b9e4e919f4c38696ee244bbb243768f8aa4346
Nishant Kumar <nishant.kumar.shreevastav@gmail.com>
2022-02-17 00:31:03 +0530

    made changes to server.js

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
-var html = require('./public/html');
-var fs = require('fs');
 
-var readStream = fs.createReadStream('./buildID', 'utf-8');
-var buildID;
 
-readStream.on('data', function (chunk) {
-	buildID = chunk;
-});
+
 let app = express();
 app.use(express.static('./public'));
 
 app.use('/', (req, res) => {
-	let Html = html(null, `animations-${buildID}`);
-	res.send(Html);
+	res.send('/public/index.html');
 });
 
 app.listen('7007', (req, res) => {
